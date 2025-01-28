8d85b86fd8270f16c0e21f4b2105af51b5cb31ab
RbxScripts663 <businesslsplash@gmail.com>
2025-01-28 08:53:21 +0000

    Create GodModeOn.lua

---
+++ GodModeOn.lua
@@ -0,0 +1,21 @@
+_G.GodMode = true
+
+while _G.GodMode == true do
+	wait()
+game.Players.LocalPlayer.Character.Head.CanTouch = false
+game.Players.LocalPlayer.Character.LeftFoot.CanTouch = false
+game.Players.LocalPlayer.Character.RightFoot.CanTouch = false
+game.Players.LocalPlayer.Character.RightHand.CanTouch = false
+game.Players.LocalPlayer.Character.LeftHand.CanTouch = false
+game.Players.LocalPlayer.Character.RightLowerLeg.CanTouch = false
+game.Players.LocalPlayer.Character.RightUpperLeg.CanTouch = false
+game.Players.LocalPlayer.Character.LeftLowerLeg.CanTouch = false
+game.Players.LocalPlayer.Character.LeftUpperLeg.CanTouch = false
+game.Players.LocalPlayer.Character.LeftLowerArm.CanTouch = false
+game.Players.LocalPlayer.Character.LeftUpperArm.CanTouch = false
+game.Players.LocalPlayer.Character.RightUpperArm.CanTouch = false
+game.Players.LocalPlayer.Character.RightLowerArm.CanTouch = false
+game.Players.LocalPlayer.Character.UpperTorso.CanTouch = false
+game.Players.LocalPlayer.Character.LowerTorso.CanTouch = false
+game.Players.LocalPlayer.Character.HumanoidRootPart.CanTouch = false
+end
